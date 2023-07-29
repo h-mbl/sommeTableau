5bf2ab4c53a16aa612cbe7c714cc37d3b3b1811b
herve.ngisse <hervembl3@gmail.com>
2023-07-29 11:59:55 -0600

    Update tp2.py

--- documents_tp2/tp2.py
+++ documents_tp2/tp2.py
@@ -14,12 +14,12 @@ valeurPng=[]
 while len(valeurPng) != len(listePng):
     #retire les elements qui ne sont pas dans l'intervalle
     valeurPng=list(filter(lambda x: x>= 1 and x<=20, list(map(lambda valeur:\
-                                               int(20*random()), range(5)))))
+                                               int(21*random()), range(5)))))
     #genere d'autres nombres pour remplacer les nombres supprimés 
     if len(valeurPng) != len(listePng) :
          valeurPng += list(filter(lambda x: x>= 1 and x<=20, list(map(lambda\
              valeur: int(20*random()), range(len(listePng)-len(valeurPng))))))
-
+print(valeurPng)
 #cette fonction dessine un tableau en html
 def tableau(hauteur,largeur):
     htmlTable= '<table>\n'
@@ -28,7 +28,7 @@ def tableau(hauteur,largeur):
       htmlTable += '<tr>\n'
       #if i != hauteur :
       for j in range(largeur):
-          pngChoice= listePng[int(4*random())]
+          pngChoice= listePng[int(5*random())]
           if i == hauteur-1 :
             if j != largeur-1:
               htmlTable += '<td id="case'+ str(compteur) + '">' '</td>\n'
@@ -97,34 +97,69 @@ def somme():
       #le saut se fait par rangée 
       saut += largeur
 
-def soustration(case,prompt):
-    print(case)
-    init= case
-    while case not in list(map(lambda x:x,range(((hauteur)*(largeur))-largeur,\
-                                                ((hauteur)*(largeur))))):
-           case +=largeur
-           
-    temp = contenu(case).innerHTML
-    valeur= int(temp)-prompt
-    contenu(case).innerHTML = str(valeur)
-    if valeur < 0 :
-        return False
-    elif valeur == 0 :
-        return True
-    colonneS=[]
-    case=init
-    print(case)
+def soustration(caseInit, valeurPrompt,tempInit):
+    #print(case)
+    #init= case
+
+    #temp = contenu(caseInit).innerHTML
+    saut=0
+    #breakpoint()
+    for i in range(largeur):
     #cette liste enregistre les valeurs des images sur une colonne
-    while case not in list(map(lambda x:x,range(largeur-1,(hauteur)*(largeur),\
-                                                largeur))):
-        case += 1
-    temp = contenu(case).innerHTML
-    valeur= int(temp)-prompt
-    contenu(case).innerHTML = str(valeur)
-    if valeur < 0 :
-        return False
-    elif valeur == 0 :
-        return True
+        colonne=[]
+        for j in range (i,(hauteur)*(largeur),largeur):
+            case= j
+            #cette condition c'est pour eviter de prendre les valeurs vides de la 
+            #dernière colonne et de la dernière rangée
+            if j != (largeur-1)*(i+1) + i and j < ((hauteur)*(largeur))-largeur :
+              #cette condition empeche de selectionner la dernière colonne lors de la
+              #derniere iteration de la premiere boucle
+              if (largeur-1)*(i+1) < ((hauteur)*(largeur))-largeur:
+                elementSvg = contenu(case).innerHTML
+                if tempInit == elementSvg: 
+                    contenu(case).innerHTML = """<div class= "container">""" +tempInit + """<h2 class='centered'>""" + str(valeurPrompt) + """</h2></div>"""
+                    #contenu(case).innerHTML = str(valeurPrompt)
+                    colonne.append(valeurPrompt)
+            #cette condition permet d'ecrire le resultat dans la derniere rangee de 
+            #la colonne en evitant la derniere colonne 
+            elif j >= ((hauteur)*(largeur))-largeur and  j < ((hauteur)*(largeur)):
+                #gestion de la condition qui empechait de selectionner la dernière 
+                #colonne lors de la derniere iteration de la premiere boucle
+                if len(colonne)>0:
+                   #breakpoint()
+                   temp = contenu(case).innerHTML
+                   valeur = int(temp)- functools.reduce(lambda x,y:\
+                                                        x+y,colonne)
+                   contenu(case).innerHTML =  str(valeur)
+                   if valeur < 0 :
+                      return False
+        #itere le saut
+        saut += largeur
+
+      #la partie suivante permet de calculer la somme de rangee
+  #initialise le saut
+    tempInit = contenu(caseInit).innerHTML
+    saut=0
+    for i in range(hauteur):
+        #cette liste enregistre les valeurs des images sur une rangée
+        rangee=[]
+        for j in range (largeur):
+            #avance d'une case
+            case= j+saut
+            if i != largeur-1 and j != hauteur-1  :
+                elementSvg = contenu(case).innerHTML
+                if tempInit == elementSvg: 
+                    #print(case)
+                    rangee.append(valeurPrompt)
+            elif j == hauteur-1 and i != largeur-1 and len(rangee)>0:
+                temp = contenu(case).innerHTML
+                valeur = int(temp)- functools.reduce(lambda x,y: x+y,rangee)
+                contenu(case).innerHTML =  str(valeur)
+                if valeur < 0 :
+                   return False
+        #le saut se fait par rangée 
+        saut += largeur
+
 def contenu(id):
    return document.querySelector("#case" + str(id))
 def clic(case):
@@ -135,7 +170,7 @@ def clic(case):
     if temp in listePng:
         contenu(case).innerHTML = """<div class= "container">""" +temp + \
         """<h2 class='centered'>""" + str(a) + """</h2></div>"""
-        soustration(case,a)
+        soustration(case,a,temp)
 def init():
     global hauteur 
     global largeur
